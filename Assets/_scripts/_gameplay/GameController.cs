6e3972674c62c7425f1b1177e8e0bb5ab5cc0f09
scm2217 <smendonc@ucsc.edu>
2015-09-05 00:12:28 -0700

    a
    a

--- Assets/_scripts/_gameplay/GameController.cs
+++ Assets/_scripts/_gameplay/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using BladeCast;
 
 public class GameController : MonoBehaviour {
 
@@ -7,4 +8,28 @@ public class GameController : MonoBehaviour {
 	public float damp = 10f;
 	public int player1Score = 0;
 	public int player2Score = 0;
+
+	float connect = 0;
+
+	//Creates all controller listeners
+	void Start () {
+		BCMessenger.Instance.RegisterListener("click",0,this.gameObject,"playerClick");
+	}
+
+	void Update () {
+		//make sure unity stays connected
+		connect += Time.deltaTime;
+		if (connect >= 5) {
+			BCMessenger.Instance.SendToListeners("check_connection", -1);
+			connect = 0;
+		}
+	}
+
+	//handles player click
+	void playerClick(ControllerMessage msg) {
+		//TODO CALL PROPER PLAYER BASED ON INDEX
+		int player = 0;
+		int.TryParse (msg.Payload.GetField ("index").ToString (), out player);
+		string button = msg.Payload.GetField ("button").ToString ();
+	}
 }
