f23563772021fb4dc41c09497c6c87da0159a10b
CatsFromMars <mduque@andrew.cmu.edu>
2015-09-05 02:52:53 -0400

    added stuff

--- Assets/_scripts/_gameplay/BeatDetector.cs
+++ Assets/_scripts/_gameplay/BeatDetector.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class BeatDetector : MonoBehaviour {
-	private Transform currentNote;
+	public Transform currentNote;
 
 	void OnTriggerStay2D(Collider2D other) {
 		if(other.gameObject.tag == "ONote" || other.gameObject.tag == "PNote") currentNote = other.gameObject.transform;
@@ -10,20 +10,5 @@ public class BeatDetector : MonoBehaviour {
 	}
 	void OnTriggerExit2D(Collider2D other) {
 		if(other.gameObject.tag == "ONote" || other.gameObject.tag == "PNote") currentNote = null;
-	}
-
-	public void hitBeat(string tag) {
-		if (currentNote != null && currentNote.tag == tag) {
-			Debug.Log ("You did it!");
-			Destroy (currentNote.gameObject);
-			currentNote = null;
-		} else
-			Debug.Log ("BOOOOO");
-	}
-
-	//Debug Controls
-	void FixedUpdate () {
-		if (Input.GetKeyDown (KeyCode.A))
-			hitBeat ("ONote");
 	}
 }
--- Assets/_scripts/_gameplay/GameController.cs
+++ Assets/_scripts/_gameplay/GameController.cs
@@ -5,6 +5,32 @@ public class GameController : MonoBehaviour {
 
 	public int bpm = 160;
 	public float damp = 10f;
-	public int player1Score = 0;
-	public int player2Score = 0;
+	Player player1;
+	Player player2;
+	public Transform b1; //Beat Detector1
+	public Transform b2; //Beat Detector2
+	public TextMesh scorep1;
+	public TextMesh scorep2;
+
+	void Awake() {
+		player1 = new Player (b1.GetComponent<BeatDetector>(), null);
+		player2 = new Player (b2.GetComponent<BeatDetector>(), null);
+	}
+
+	void takeInput(int playerIndex, string tag) {
+		//Tag has to either be ONote or PNote
+		if (playerIndex == 1)
+			player1.hit (tag);
+		else if (playerIndex == 2)
+			player2.hit (tag);
+	}
+
+	void FixedUpdate() {
+		if(Input.GetKeyDown(KeyCode.A)) takeInput(1, "ONote");
+		if(Input.GetKeyDown(KeyCode.S)) takeInput(1, "PNote");
+
+		//Score
+		scorep1.text = "Player 1 Score: " + player1.score.ToString ();
+		scorep2.text = "Player 2 Score: " + player2.score.ToString ();
+	}
 }
